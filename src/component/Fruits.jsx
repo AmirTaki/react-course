150f635df2d0626141e85ae5339c66fef505c02e
amir <amirtaki623@gmail.com>
2025-06-07 18:24:51 +0330

    Rendering Array Of Objects In React

--- src/component/Fruits.jsx
+++ src/component/Fruits.jsx
@@ -1,15 +1,17 @@
 export default function Fruits (){
-    const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+    const fruits = [
+        { name : "apple", price : 10, emoji : "🍎" },
+        { name : "Mango", price : 7, emoji : "🥭" },
+        { name : "Banana", price : 2, emoji : "🍌" },
+        { name : "Orange", price : 5, emoji : "🍊" },
+        { name : "Pineapple", price : 8, emoji : "🍍" }
+    ]
     return (
     <>  
         <ul>
-            {
-                fruits.map((fruit)=>(
-                    <li key = {fruit}> 
-                        {fruit}
-                    </li>
-                ))
-            }
+            {fruits.map((fruit)=>(
+                <li key = {fruit.name}> {fruit.emoji} {fruit.name} ${fruit.price} </li>
+            ))}
         </ul>
     </>
     )
