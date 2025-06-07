d61f7595172b85ed14f53c92914183ee24027371
amir <amirtaki623@gmail.com>
2025-06-07 17:42:31 +0330

    Passing Arrays & Objects To Components Using

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import Hello from "./component/Hello"
+import { Another } from "./component/Hello";
 function App() {
+  const seatNumbers = [1, 4, 7];
+  const person =  {
+    name : "amir",
+    message : "Hi there !",
+    emoji : "👋",
+    seatNumbers : [1, 4, 7]
+  }
   return (
     <>
       <div>
-        <Hello name = "amir" message = "hi there" emoji = "👋"/>
+        <Hello 
+          name = "amir"
+          message = "hi there" 
+          emoji = "👋"
+          seatNumbers = {seatNumbers}
+        />
+        <Another person = {person} />
       </div>
     </>
   )
--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,9 +1,19 @@
-function Hello(props) {
-    props.name = "mani"; // warning : props is Immutability
+function Hello({message, emoji, name, seatNumbers} ) {
     return (
         <>
-            <h1>{props.message} {props.emoji} {props.name} </h1>
+            <h1>{message} {emoji} {name} {seatNumbers}</h1>
         </>
     )
 }
+
+export const Another = (props) =>{
+    return(
+        <>
+            <h1>{props.person.message} {props.person.emoji} {props.person.name} {props.person.seatNumbers}</h1>
+        </>
+    )
+}
+
+
+
 export default Hello;
