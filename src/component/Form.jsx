badf3fa3ebe9cc9dd37a1f47da16937123a7d3c1
amir <amirtaki623@gmail.com>
2025-06-08 03:44:50 +0330

    Handling Form Submission In React

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 
 export default function Form(){
     const [name, setName]  = useState({firstName : "", lastName : ""})
+    function handelSubmit(e){
+        e.preventDefault();
+        console.log(name)
+    }
     return(
         <>
         <div>
@@ -10,6 +14,7 @@ export default function Form(){
         <form>
             <input onChange={(e)=> setName({...name, firstName : e.target.value })} type="text" value = {name.firstName}  placeholder="enter the name"/>
             <input onChange={(e)=> setName({...name, lastName : e.target.value})} type="text" value = {name.lastName} placeholder="enter the family" />
+            <button onClick={(e)=>handelSubmit(e)}>Add</button>
         </form>
         </>
     )
