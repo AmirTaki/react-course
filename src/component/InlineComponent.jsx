636a95df8c62014b635d9111e6f9fc248cb8e35b
amir <amirtaki623@gmail.com>
2025-06-08 19:53:23 +0330

    CSS Stylesheets To Style Components

--- src/component/InlineComponent.jsx
+++ src/component/InlineComponent.jsx
@@ -1,10 +1,8 @@
-const header  = {color : 'red', backgroundColor : "black"}
+import  "../css/style.css"
 export default function InlineComponent(){
     return(
         <>
-            <h1 style={{color : 'blue', fontSize : '140px'}}>Inline Component</h1>
-            <h1 style={header}>Inline Component</h1>
-
+            <h1 className="header">Inline Component</h1>
         </>
     )
 }
