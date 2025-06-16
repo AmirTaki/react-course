8c8f851d5be7c7cebc10bb9488bed3bafcd29bfb
amir <amirtaki623@gmail.com>
2025-06-16 03:58:36 +0330

    Using Complex Object As State In useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ function App() {
   // const [nameState, disptach] = useReducer (nameFucntion, value);
   // function reducer(state, action) { condition action => state action.payload }
   
-  const [state, dispatch] = useReducer(reducer, {count : 0});
+  const [state, dispatch] = useReducer(reducer, {count : 0, incrementBy : 0});
   function reducer (state, action) {
       if (action.type === "increment"){
-        return {...state, count : state.count + action.payload }
+        return {...state, count : state.count + state.incrementBy }
       }
 
-      else if (action.type === "decrement"){
-        return {...state, count : state.count - action.payload }
-        // return state - action.payload
+      if (action.type === "decrement"){
+        return {...state, count : state.count - state.incrementBy }
+      }
+      if(action.type === "setIncrement"){
+        return{...state,  incrementBy: action.paylod}
       }
   }
   return (
     <>
-      <p> use Reducer : {state.count}</p>
-      <button onClick = {()=>{dispatch({type : 'increment', payload : 1})}}>Increment count</button>
-      <button onClick = {()=>{dispatch({type : 'decrement', payload : 1})}}>Decrement count</button>
+      <p> use Reducer : {state.count} </p>
+      <input type="text" value = {state.incrementBy}  onChange={(e)=>{dispatch({type : "setIncrement", paylod : Number(e.target.value)})}}/>
+      <button onClick = {()=>{dispatch({type : 'increment'})}}>Increment count</button>
+      <button onClick = {()=>{dispatch({type : 'decrement'})}}>Decrement count</button>
     </>
   )
 }
