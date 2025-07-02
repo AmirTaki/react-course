b22f65a613236e72b8d10603399bf3bbf9a2b5ce
amir <amirtaki623@gmail.com>
2025-07-02 22:16:46 +0330

    Link Component In React

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Dashboard from "./pages/Dashboard"
 import PageNotFound from "./pages/PageNotFound"
+import Navbar from "./Navbar"
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path = "/" element = {<Homepage/>}  />  
         <Route path = "/dashboard" element ={<Dashboard />} />
