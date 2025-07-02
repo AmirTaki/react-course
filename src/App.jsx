465ab30d445cd46e815ca51908a8d25afd091ff5
amir <amirtaki623@gmail.com>
2025-07-02 21:03:56 +0330

    React Router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { useReducer, useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Dashboard from "./pages/Dashboard"
+import PageNotFound from "./pages/PageNotFound"
 
 function App() {
-    const [amount, setAmount] = useState(0)
-    const [state, dispatch] = useReducer(reducer, {balance: 0});
-    function reducer(state, action){
-      if(action.type === "add"){
-        return {...state, balance : state.balance +  action.payload}
-      }
-      if(action.type === "sub"){
-        return {...state, balance : state.balance - action.payload}
-      }
-    }
+
   return (
-    <>
-      {state.balance}
-      <input type="text" value = {amount} onChange={(e)=>{setAmount(e.target.value)}}/>
-      <button onClick={()=>dispatch({type : "add", payload : Number (amount)})}>
-        Deposit
-      </button>
-      <button  onClick={()=>dispatch({type : "sub", payload : Number(amount)})}>
-        Withdraw
-      </button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Homepage/>}  />  
+        <Route path = "/dashboard" element ={<Dashboard />} />
+        <Route path = "*" element = {<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
