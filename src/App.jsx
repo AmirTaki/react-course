db968a123d6f1e787f58afa134829e6e38e485cc
amir <amirtaki623@gmail.com>
2025-07-06 18:36:04 +0330

    course complete react vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import Login from "./pages/Login"
-
+import styled from "./css/styles.module.css"
 function App() {
   
   return (
-  <>
-    <Login />
-  </>
+  <div className = {styled.caption}>
+    <div>c3781c2  ==> Creating Our Own useLocalStorage Hook In ReactJS 
+    </div><div> 049ff8a   ==>  Creating Custom Hooks In React
+    </div><div> 7f891a4   ==>  Passing Complex State To Reducer
+    </div><div> 363a253   ==>  Using Reducer With Context Provider In React
+    </div><div> b8b4f93   ==>  Sharing Context With Other Components In React
+    </div><div> 4e8d85c   ==>  Separating Context Into Its Own File In React
+    </div><div> 29d3b96   ==>  Context API In React
+    </div><div> 0885ff4   ==>  useNavigate Hook In React
+    </div><div> fa14ea7   ==>  React Router Dom : Dynamic Routes
+    </div><div> 152968a   ==>   Nesting Routes
+    </div><div> b22f65a   ==>  Link Component In React
+    </div><div> 465ab30   ==>  React Router dom
+    </div><div> 368d174   ==>  Understanding Reducer Using Bank Account Example
+    </div><div> 8c8f851   ==>  Using Complex Object As State In useReducer
+    </div><div> cf0fba9   ==>  Using Object As State In useReducer
+    </div><div> 89f72f7   ==>  useReducer Hook In React
+    </div><div> f44be5b   ==>  Syntax of the useEffecct hook
+    </div><div> 39371b6   ==>  CSS Modules For Styling
+    </div><div> 636a95d   ==>   CSS Stylesheets To Style Components
+    </div><div> 83db709   ==>  Inline Styling In React Components
+    </div><div> badf3fa   ==>  Handling Form Submission In React
+    </div><div> 67ba1ab   ==>  Handling Multiple Inputs In React
+    </div><div> 6a91b72   ==>  Handling Input Fields In React
+    </div><div> a46b914   ==>  Creating Multiple States In React
+    </div><div> 3a39f83   ==>  State in React
+    </div><div> 607cb05   ==>  Event Handling In React
+    </div><div> ca1e31a   ==>  Conditionally Rendering A Message Using Ternary
+    </div><div> 72ca424   ==>  Conditionally Rendering List Items
+    </div><div> 44ea043   ==>  Using Ternary Operators In React
+    </div><div> af549bc   ==>  Conditional Rendering Using Element Variables
+    </div><div> 853af13   ==>  Conditionally Rendering JSX & Components
+    </div><div> 9bf52da   ==>  Rendering Components Inside A Loop
+    </div><div> 150f635   ==>  Rendering Array Of Objects In React
+    </div><div> 489a60c   ==>   Rendering Arrays Or Lists In React
+    </div><div> d61f759   ==>  Passing Arrays & Objects To Components Using
+    </div><div> d7de879   ==>  Immutability of Props
+    </div><div> 1e06dfa   ==>  Destructuring Props
+    </div><div> 6e54bca   ==>  What are Props In React
+    </div><div> a06b325   ==>  Reusability of Components In React
+    </div><div> 5befb6e   ==>  What is JSX In React
+    </div><div> 0c2a160   ==>  Creating A Component In React
+    </div><div> 417c614   ==>  How React App Works
+    </div><div>939762d    ==> create folders react + vite (toturial)`</div>
+  </div>
   )
 }
 
